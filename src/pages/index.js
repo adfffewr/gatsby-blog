c5d98b8950f8bb342a73db29e4ec485324552730
이성재 <adfffewr@naver.com>
2020-03-28 15:31:31 +0900

    index page 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,27 +2,58 @@ import React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
 import styled from 'styled-components';
 import Layout from '../components/layout';
-// import Image from '../components/image';
-import SEO from '../components/seo';
+import { palette } from '../components/common/GlobalStyles';
 
 const Container = styled.div`
-  color: red;
-  font-size: 30px;
+  display: block;
+  h1 {
+    font-size: 2.5rem;
+    font-weight: bold;
+    margin-bottom: 2rem;
+  }
 `;
+
+const List = styled.li`
+  & + li {
+    margin-top: 1.5rem;
+  }
+  a {
+    color: ${palette.white};
+  }
+  a:hover {
+    color: ${palette.green};
+  }
+`;
+const Date = styled.time`
+  font-size: 0.8rem;
+  font-weight: bold;
+  color: ${palette.green};
+`;
+const Title = styled.h2`
+  font-size: 1.1rem;
+  font-weight: bold;
+  margin: 5px 0;
+`;
+// const TextContent = styled.div`
+//   p {
+//     font-size: 0.9rem;
+//     font-weight: bold;
+//   }
+// `;
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      allMarkdownRemark {
+      allMarkdownRemark(sort: { order: DESC, fields: frontmatter___date }) {
         edges {
           node {
             id
+            fields {
+              slug
+            }
             frontmatter {
               title
               date
-            }
-            fields {
-              slug
             }
           }
         }
@@ -33,27 +64,23 @@ const IndexPage = () => {
 
   return (
     <Layout>
-      <SEO title="Home" />
-      <Container>안녕하세요!</Container>
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        {/* <Image /> */}
-      </div>
-      <Link to="/page-2/">Go to page 2</Link>
-      <ol>
-        {data.allMarkdownRemark.edges.map(edge => {
-          return (
-            <li key={edge.node.id}>
-              <Link to={`/post/${edge.node.fields.slug}`}>
-                <h2>{edge.node.frontmatter.title}</h2>
-                <p>{edge.node.frontmatter.date}</p>
-              </Link>
-            </li>
-          );
-        })}
-      </ol>
+      <Container>
+        <h1>Home</h1>
+        <ol>
+          {data.allMarkdownRemark.edges.map(edge => {
+            return (
+              <List key={edge.node.id}>
+                <Link to={`/post/${edge.node.fields.slug}`}>
+                  <Date datetime={edge.node.frontmatter.date}>
+                    {edge.node.frontmatter.date}
+                  </Date>
+                  <Title>{edge.node.frontmatter.title}</Title>
+                </Link>
+              </List>
+            );
+          })}
+        </ol>
+      </Container>
     </Layout>
   );
 };
